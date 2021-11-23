468caa2e55525bd9172389e701d7c9d1c3b42144
MacdonaldRobinson <maocnly2001@gmail.com>
2021-11-22 18:53:43 -0700

    Updated export

--- Overlay.gd
+++ Overlay.gd
@@ -17,13 +17,20 @@ func _on_RedCandy_gift_collected(gift, collect_sound):
 func _on_Nude_gift_collected(gift, collect_sound):
 	nude_count.text = String(int(nude_count.text) + 1)
 	get_node(collect_sound).play()
-		
+
+
+func resume_game():
+	get_tree().paused = false	
+
+func pause_game():
+	get_tree().paused = true
+
 func _on_JamesonAssist_gift_collected(gift, collect_sound):
 	if is_being_assisted:
 		return
-	
+
 	get_node(collect_sound).play()
-		
+
 	jameson_assist_count.text = String(int(jameson_assist_count.text) + 1)
 			
 	jameson_assist_popup.show()	
@@ -32,8 +39,6 @@ func _on_JamesonAssist_gift_collected(gift, collect_sound):
 	is_being_assisted = true
 
 func _on_JamesonAssistPopup_timeout():
-	animation_player.play_backwards("jameson_assist_popup")
-	yield(get_tree().create_timer(1), "timeout")
 	jameson_assist_popup.hide()
 		
 func _on_JamesonAssistTimer_timeout():
